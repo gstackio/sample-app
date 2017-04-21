d01ee91b53e7fc223eaf00a447faaa6b345e94f1
Benjamin Gandon <benjamin@gstack.io>
2017-04-21 10:05:28 +0200

    Let's add a bit of Gstack marketing

--- handlers/hello.go
+++ handlers/hello.go
@@ -17,7 +17,7 @@ func (p *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	styledTemplate.Execute(w, Body{Body: fmt.Sprintf(`
 <div class="hello">
-	Test App	
+	Gstack rocks!
 </div>
 
 <div class="my-index">My Index Is</div>
